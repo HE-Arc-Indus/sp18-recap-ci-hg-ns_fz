ca10217f580eca86d4777a22cb35bfdabe5e2696
nsutterlet <n.sutterlet@outlook.com>
2018-05-10 16:24:11 +0200

    FizzBuzz add test checkOneToHundred

--- test/java/ch/hearc/ig/indus/fizzbuzz/FizzBuzzTest.java
+++ test/java/ch/hearc/ig/indus/fizzbuzz/FizzBuzzTest.java
@@ -3,8 +3,19 @@ package ch.hearc.ig.indus.fizzbuzz;
 import org.junit.Before;
 
 public class FizzBuzzTest {
-
+  FizzBuzz fb;
+  String[] testFb;
   @Before
   public void setUp() throws Exception {
+    fb = new FizzBuzz();
+
   }
+  @Test
+  public void checkOneToHundred() {
+
+    for (Integer i=1;i<=100;i++){
+      assertEquals("nombre suit la séquence", i.toString(), fb.getMaListe()[i-1]);
+    }
+  }
+
 }
